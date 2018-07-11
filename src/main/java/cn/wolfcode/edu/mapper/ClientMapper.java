90d59e9dae7b1aa1303fd9fb872991508e98c87f
hong <hong@qq.com>
2018-07-11 15:55:17 +0800

    chenfen 第一次提交 1>完成Client 潜在客户的增删改查 2>添加权限命名的注解工具类PermissionName

---
+++ src/main/java/cn/wolfcode/edu/mapper/ClientMapper.java
@@ -0,0 +1,22 @@
+package cn.wolfcode.edu.mapper;
+
+import cn.wolfcode.edu.domain.Client;
+import cn.wolfcode.edu.query.ClientQueryObject;
+
+import java.util.List;
+
+public interface ClientMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Client record);
+
+    Client selectByPrimaryKey(Long id);
+
+    List<Client> selectAll();
+
+    int updateByPrimaryKey(Client record);
+
+    int queryForCount(ClientQueryObject qo);
+
+    List<Client> queryForList(ClientQueryObject qo);
+}
---
+++ src/main/java/cn/wolfcode/edu/web/controller/ClientController.java
@@ -0,0 +1,84 @@
+package cn.wolfcode.edu.web.controller;
+
+import cn.wolfcode.edu.domain.Client;
+import cn.wolfcode.edu.query.ClientQueryObject;
+import cn.wolfcode.edu.query.PageResult;
+import cn.wolfcode.edu.service.IClientService;
+import cn.wolfcode.edu.util.JsonResult;
+import cn.wolfcode.edu.util.PermissionName;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("client")
+public class ClientController {
+    @Autowired
+    private IClientService clientService;
+
+    @RequestMapping("")
+    @RequiresPermissions("client:index")
+    @PermissionName("潜在客户首页")
+    public String index() {
+        return "client/list";
+    }
+
+    @RequestMapping("list")
+    @ResponseBody
+    @RequiresPermissions("client:list")
+    @PermissionName("潜在客户列表")
+    public PageResult list(ClientQueryObject qo) {
+        PageResult result = clientService.query(qo);
+        return result;
+    }
+
+    @RequestMapping("save")
+    @ResponseBody
+    @RequiresPermissions("client:save")
+    @PermissionName("潜在客户保存")
+    public JsonResult save(Client client) {
+        JsonResult result = new JsonResult();
+        try {
+            clientService.save(client);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.markMsg("保存失败");
+        }
+        return result;
+    }
+
+    @RequestMapping("/update")
+    @ResponseBody
+    @RequiresPermissions("client:update")
+    @PermissionName("潜在客户更新")
+    public JsonResult update(Client client) {
+        JsonResult result = new JsonResult();
+        try {
+            clientService.update(client);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.markMsg("更新失败");
+        }
+        return result;
+    }
+
+    @RequestMapping("delete")
+    @ResponseBody
+    @RequiresPermissions("client:delete")
+    @PermissionName("潜在客户删除")
+    //没有这个方法
+    public JsonResult delete(Long id) {
+        JsonResult result = new JsonResult();
+        try {
+            clientService.delete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.markMsg("删除失败");
+        }
+        return result;
+    }
+
+
+}
