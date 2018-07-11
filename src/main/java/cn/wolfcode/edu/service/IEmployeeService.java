470cf37717635b1e4184fd24a1645182423f4da3
lijuntao <312948247@qq.com>
2018-07-11 10:53:32 +0800

    公用的文件更改了comons.jsp文件 上传了Employee类

--- src/main/java/cn/wolfcode/edu/service/IEmployeeService.java
+++ src/main/java/cn/wolfcode/edu/service/IEmployeeService.java
@@ -1,16 +1,47 @@
 package cn.wolfcode.edu.service;
 
 import cn.wolfcode.edu.domain.Employee;
+import cn.wolfcode.edu.query.PageResult;
+import cn.wolfcode.edu.query.QueryObject;
 
 import java.util.List;
 
-/**
- * Created by WangZhe on 2018/7/10.
- */
 public interface IEmployeeService {
     /**
-     * 返回所有员工
-     * @return  员工列表
+     * 删除员工
+     * @param id 待删除员工的id
      */
-    List<Employee> list();
+    void deleteByPrimaryKey(Long id);
+
+    /**
+     * 添加员工
+     * @param record
+     */
+    void insert(Employee record);
+
+    /**
+     * 查询单个员工
+     * @param id  待查询员工的id
+     * @return
+     */
+    Employee selectByPrimaryKey(Long id);
+
+    /**
+     * 查询所有的员工
+     * @return
+     */
+    List<Employee> selectAll();
+
+    /**
+     * 更新员工
+     * @param record
+     */
+    void updateByPrimaryKey(Employee record);
+
+    /**
+     * 查询到分页结果集
+     * @param qo 查询参数对象
+     * @return
+     */
+    PageResult query(QueryObject qo);
 }
