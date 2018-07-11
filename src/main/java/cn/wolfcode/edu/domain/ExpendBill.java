99bbe4e29be6d3075260fdbe9c2092066385546c
zhangke <zhangke@qq.com>
2018-07-11 12:53:03 +0800

    支出的service与controller list页面的书写

--- src/main/java/cn/wolfcode/edu/domain/ExpendBill.java
+++ src/main/java/cn/wolfcode/edu/domain/ExpendBill.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class ExpendBill {
     private Long id;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH-mm-ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date payTime;
 
     private BigDecimal payMoney;
