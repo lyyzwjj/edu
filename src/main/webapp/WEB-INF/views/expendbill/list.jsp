fbb73dbce5cf1bd849f04c4dc118724ed0b746e8
zhangke <zhangke@qq.com>
2018-07-11 13:37:57 +0800

    支出页面的增删改查弹窗

--- src/main/webapp/WEB-INF/views/expendbill/list.jsp
+++ src/main/webapp/WEB-INF/views/expendbill/list.jsp
@@ -9,5 +9,53 @@
 </head>
 <body>
     <table id="exp_datagruid"></table>
+
+    <div id="tb">
+        <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-add',plain:true" data-cmd="add">添加</a>
+        <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-remove',plain:true" data-cmd="remove">删除</a>
+        <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-reload',plain:true" data-cmd="reload">刷新</a>
+        <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-search',plain:true" data-cmd="search">高级查询</a>
+        <a href="#" class="easyui-linkbutton" data-options="iconCls:'icon-ok',plain:true" data-cmd="ok">审核</a>
+    </div>
+
+    <div id="exp_dialog">
+        <form id="editForm" method="post" style="padding: 40px 0px 0px 30px;">
+            <input name="id" id="expId" type="hidden">
+            <table style="border-collapse:separate; border-spacing:10px;">
+                <tr>
+                    <td><font size="1">用户名:</font></td>
+                    <td><input class="easyui-textbox" type="text" name="username"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">真实姓名:</font></td>
+                    <td><input class="easyui-textbox" type="text" name="realname"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">密码:</font></td>
+                    <td><input class="easyui-textbox" type="text" name="password"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">电话:</font></td>
+                    <td><input class="easyui-textbox" type="text" name="tel"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">邮箱:</font></td>
+                    <td><input class="easyui-textbox" type="text" name="email"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">入职日期:</font></td>
+                    <td><input class="easyui-datebox" type="text" name="hiredate"/></td>
+                </tr>
+                <tr>
+                    <td><font size="1">是否为超级管理员:</font></td>
+                    <td><input class="easyui-combobox" type="text" name="admin" id="adminId"
+                               data-options="data:[{'value':1, 'text':'是'}
+                                ,{'value':0, 'text':'否'}], valueField:'value',
+                                textField:'text',panelHeight:'auto'"
+                    /></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </body>
 </html>
--- src/main/webapp/static/js/system/expendbill.js
+++ src/main/webapp/static/js/system/expendbill.js
@@ -1,5 +1,9 @@
 $(function(){
-    $('#exp_datagruid').datagrid({
+    var exp_datagruid = $('#exp_datagruid');
+    var exp_dialog = $("#exp_dialog");
+
+    //datagruid的初始化
+    exp_datagruid.datagrid({
         url:"/expendbill/list",
         fitColumns:true,
         striped:true,
@@ -40,6 +44,101 @@ $(function(){
                 }
             }
             }
-        ]]
+        ]],
+        toolbar:"#tb",
     });
+    //end
+
+    //exp_dialog的初始化
+    exp_dialog.dialog({
+        title: "温馨提示",
+        width: 400,
+        height: 400,
+        top: 100,
+        buttons: "#bb",
+        closed: true
+    })
+    //end
+
+    var cmdObj = {
+
+        //刷新操作
+        reload: function () {
+            exp_datagruid.datagrid("load");
+        },
+
+        //取消按钮
+        cancel: function () {
+            exp_dialog.dialog("close");
+        },
+
+        //查询操作
+        query: function () {
+            var keyword = $("#keyword").textbox("getText");
+            var beginDate = $("#beginDate").datebox("getText");
+            var endDate = $("#endDate").datebox("getText");
+            exp_datagruid.datagrid("load", {
+                keyword: keyword,
+                beginDate: beginDate,
+                endDate: endDate
+            });
+        },
+
+        //保存操作
+        save: function () {
+            var id = $("#expId").val();
+            controller = "/expendbill/save";
+            if (id) {
+                var controller = "/expendbill/update";
+            }
+            $("#editForm").form("submit", {
+                url: controller,
+                /*onSubmit: function (param) {
+                    var ids = $("#rolesId").combobox("getValues");
+                    for (var i = 0; i < ids.length; i++) {
+                        param["roles[" + i + "].id"] = ids[i];
+                    }
+                },*/
+                success: function (data) {
+                    data = $.parseJSON(data);
+                    if (!data.success) {
+                        $.messager.alert('温馨提示', data.msg);
+                    } else {
+                        exp_dialog.dialog("close");
+                        exp_datagrid.datagrid("reload");
+                    }
+                }
+            })
+        },
+
+        //删除
+        remove: function () {
+            var row = exp_datagrid.datagrid("getSelected");
+            if (!row) {
+                $.messager.alert("温馨提示", "请选择要删除的行")
+            } else {
+                $.get("/expendbill/delete", {id: row.id}, function (data) {
+                    if (!data.success) {
+                        $.messager.alert("温馨提示", data.msg)
+                    } else {
+                        exp_dialog.dialog("close");
+                        exp_datagrid.datagrid("reload");
+                    }
+                })
+            }
+        },
+
+        //添加
+        add: function () {
+            $("#editForm").form("clear");
+            exp_dialog.dialog("open");
+            exp_dialog.dialog("setTitle", "员工添加");
+        }
+    }
+
+    $("a[data-cmd]").click(function () {
+        var cmd = $(this).data("cmd");
+        cmdObj[cmd]();
+    })
 })
+
