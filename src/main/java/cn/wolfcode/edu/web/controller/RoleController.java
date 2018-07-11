06b3bed3dfcb2df3201bc167b82c04fab6390a85
lwj <@qq1061623923.com>
2018-07-11 20:25:04 +0800

    提交部门基本功能

--- src/main/java/cn/wolfcode/edu/web/controller/RoleController.java
+++ src/main/java/cn/wolfcode/edu/web/controller/RoleController.java
@@ -1,9 +1,9 @@
 package cn.wolfcode.edu.web.controller;
 
-import cn.wolfcode.edu.domain.Employee;
+import cn.wolfcode.edu.domain.Role;
 import cn.wolfcode.edu.query.DepartmentQueryObject;
 import cn.wolfcode.edu.query.PageResult;
-import cn.wolfcode.edu.service.IEmployeeService;
+import cn.wolfcode.edu.service.IRoleService;
 import cn.wolfcode.edu.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,29 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("employee")
-public class EmployeeController {
+@RequestMapping("role")
+public class RoleController {
     @Autowired
-    private IEmployeeService employeeService;
+    private IRoleService roleService;
 
     @RequestMapping("")
     public String index() {
-        return "employee/list";
+        return "role/list";
     }
 
     @RequestMapping("list")
     @ResponseBody
     public PageResult list(DepartmentQueryObject qo) {
-        PageResult result = employeeService.query(qo);
+        PageResult result = roleService.query(qo);
         return result;
     }
 
     @RequestMapping("/save")
     @ResponseBody
-    public JsonResult save(Employee employee) {
+    public JsonResult save(Role role) {
         JsonResult result = new JsonResult();
         try {
-            employeeService.save(employee);
+            roleService.save(role);
         } catch (Exception e) {
             e.printStackTrace();
             result.markMsg("保存失败");
@@ -43,10 +43,10 @@ public class EmployeeController {
 
     @RequestMapping("/update")
     @ResponseBody
-    public JsonResult update(Employee employee) {
+    public JsonResult update(Role role) {
         JsonResult result = new JsonResult();
         try {
-            employeeService.update(employee);
+            roleService.update(role);
         } catch (Exception e) {
             e.printStackTrace();
             result.markMsg("更新失败");
@@ -59,7 +59,7 @@ public class EmployeeController {
     public JsonResult delete(Long id) {
         JsonResult result = new JsonResult();
         try {
-            employeeService.delete(id);
+            roleService.delete(id);
         } catch (Exception e) {
             e.printStackTrace();
             result.markMsg("删除失败");
